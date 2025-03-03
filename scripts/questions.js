5f6fad0fe5da0a06353d6f46d7c7f1789e70b775
Felix Wagner <post@felixwagner.eu>
2025-03-03 09:15:38 +0100

    feat:add color for selected answer

---
+++ scripts/questions.js
@@ -0,0 +1,46 @@
+let questions = [
+  {
+    question:
+      "Welche dieser Schichten der Erdatmosphäre ist der Erdoberfläche am nächsten?",
+    answer_1: "Troposphäre",
+    answer_2: "Stratosphäre",
+    answer_3: "Mesosphäre",
+    answer_4: "Ionosphäre",
+    correct_answer: 1,
+  },
+  {
+    question: "Was ist die initiale Hauptursache für Wind auf der Erde?",
+    answer_1: "Die Rotationsgeschwindigkeit der Erde",
+    answer_2: "Die Neigung der Erdachse",
+    answer_3: "Unterschiede im Luftdruck",
+    answer_4: "Reibungswiderstand der Erdoberfläche",
+    correct_answer: 3,
+  },
+  {
+    question:
+      "Welche der folgenden Wolkenarten wird am häufigsten mit Gewittern in Verbindung gebracht?",
+    answer_1: "Cumulonimbus",
+    answer_2: "Altocumulus",
+    answer_3: "Status",
+    answer_4: "Cirrus",
+    correct_answer: 1,
+  },
+  {
+    question:
+      "In welcher Richtung zirkuliert ein Hochdruckgebiet auf der Nordhalbkugel um seinen Kern?",
+    answer_1: "Im Uhrzeigersinn (mathematisch antizyklonal)",
+    answer_2: "Gegen den Uhrzeigersinn (mathematisch zyklonal)",
+    answer_3: "Im Sommer zyklonal, im Winder antizyklonal",
+    answer_4: "Tagsüber zyklonal, nachts antizyklonal",
+    correct_answer: 1,
+  },
+  {
+    question:
+      "Warum werden Winde auf der Nordhalbkugel in Strömungsrichtung nach rechts abgelenkt? Wegen ...",
+    answer_1: "der Druckgradientkraft",
+    answer_2: "der Corioliskraft",
+    answer_3: "der Gravitationskraft",
+    answer_4: "dem Reibunskraft an der Erdoberfläche",
+    correct_answer: 2,
+  },
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,34 @@
-let questions = [
-  {
-    question:
-      "Welche dieser Schichten der Erdatmosphäre ist der Erdoberfläche am nächsten?",
-    answer_1: "Troposphäre",
-    answer_2: "Stratosphäre",
-    answer_3: "Mesosphäre",
-    answer_4: "Ionosphäre",
-    correct_answer: 1,
-  },
-  {
-    question: "Was ist die initiale Hauptursache für Wind auf der Erde?",
-    answer_1: "Die Rotationsgeschwindigkeit der Erde",
-    answer_2: "Die Neigung der Erdachse",
-    answer_3: "Unterschiede im Luftdruck",
-    answer_4: "Reibungswiderstand der Erdoberfläche",
-    correct_answer: 3,
-  },
-  {
-    question:
-      "Welche der folgenden Wolkenarten wird am häufigsten mit Gewittern in Verbindung gebracht?",
-    answer_1: "Cumulonimbus",
-    answer_2: "Altocumulus",
-    answer_3: "Status",
-    answer_4: "Cirrus",
-    correct_answer: 1,
-  },
-  {
-    question:
-      "In welcher Richtung zirkuliert ein Hochdruckgebiet auf der Nordhalbkugel um seinen Kern?",
-    answer_1: "Im Uhrzeigersinn (mathematisch antizyklonal)",
-    answer_2: "Gegen den Uhrzeigersinn (mathematisch zyklonal)",
-    answer_3: "Im Sommer zyklonal, im Winder antizyklonal",
-    answer_4: "Tagsüber zyklonal, nachts antizyklonal",
-    correct_answer: 1,
-  },
-  {
-    question:
-      "Warum werden Winde auf der Nordhalbkugel in Strömungsrichtung nach rechts abgelenkt? Wegen ...",
-    answer_1: "der Druckgradientkraft",
-    answer_2: "der Corioliskraft",
-    answer_3: "der Gravitationskraft",
-    answer_4: "dem Reibunskraft an der Erdoberfläche",
-    correct_answer: 2,
-  },
-];
+let currentQuestionIndex = 0;
+
+function init() {
+  setTotalQuestionsNumber(questions);
+  showCurrentQuestion();
+}
+
+function setTotalQuestionsNumber(array) {
+  contentRef = document.getElementById("total_questions_number");
+  contentRef.innerHTML = String(array.length);
+}
+
+function showCurrentQuestion() {
+  let question = questions[currentQuestionIndex];
+  document.getElementById("question_text").innerHTML = String(
+    question.question
+  );
+  document.getElementById("answer_1").innerHTML = String(question.answer_1);
+  document.getElementById("answer_2").innerHTML = String(question.answer_2);
+  document.getElementById("answer_3").innerHTML = String(question.answer_3);
+  document.getElementById("answer_4").innerHTML = String(question.answer_4);
+}
+
+function answer(answerIndex) {
+  let question = questions[currentQuestionIndex];
+  let answerId = "answer_" + String(answerIndex);
+  let answerParentNodeRef = document.getElementById(answerId).parentNode;
+
+  if (answerIndex == question.correct_answer) {
+    answerParentNodeRef.classList.add("bg-success");
+  } else {
+    answerParentNodeRef.classList.add("bg-danger");
+  }
+}
